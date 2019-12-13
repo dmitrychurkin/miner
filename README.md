e195e31c447050f32ac658b08b3d4bc4d80a7dd5
dmitry <dmitrychurkin1988@gmail.com>
2019-12-13 19:20:52 +0200

    refactored implementation, added launch description to readme.md

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 `-host` - hosting service (default https://faucet.ropsten.be/donate)   
 `-addr` - etherium wallet address   
 `-d` - poll timeout (default 10s)   
+
+#### Пример запуска   
+> miner.exe -addr=0xeA762878a8Dd131Ecfc46c6A45ED9F1EE9d095B3 -d=1000ns
--- main.go
+++ main.go
@@ -10,31 +10,36 @@ import (
 )
 
 func main() {
-	host := flag.String("host", "https://faucet.ropsten.be/donate", "Defines a remote host of maining service")
-	walletAddress := flag.String("addr", "0xeA762878a8Dd131Ecfc46c6A45ED9F1EE9d095B3", "MM wallet address")
-	duration := flag.Duration("d", 10, "Timeout between ticks")
+	var (
+		host          = flag.String("host", "https://faucet.ropsten.be/donate", "Remote host of mining service")
+		walletAddress = flag.String("addr", "0xeA762878a8Dd131Ecfc46c6A45ED9F1EE9d095B3", "Wallet address")
+		duration      = flag.Duration("d", 10, "Poll interval")
+	)
+
 	flag.Parse()
 
 	fmt.Println(*host, *walletAddress, *duration)
+
+	sendRequest := func(host, walletAddress *string) {
+		resp, err := http.Get(*host + "/" + *walletAddress)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Println(string(body))
+	}
+
+	go sendRequest(host, walletAddress)
 
 	for range time.Tick(*duration * time.Second) {
 		go sendRequest(host, walletAddress)
 	}
 
-}
-
-func sendRequest(host, walletAddress *string) {
-	resp, err := http.Get(*host + "/" + *walletAddress)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Println(string(body))
 }
